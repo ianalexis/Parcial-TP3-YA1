e62ae2d8c4e24a0349a183c569cd112ad4d6924e
Franco Leonel Cristillo <franco.frlcr@gmail.com>
2024-05-28 23:10:32 -0300

    Cards y RV

--- app/src/main/java/com/example/parcial_tp3_ya1/fragments/Explore.kt
+++ app/src/main/java/com/example/parcial_tp3_ya1/fragments/Explore.kt
@@ -5,28 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.parcial_tp3_ya1.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Explore.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Explore : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    lateinit var view : View
+    lateinit var rvDestinations : RecyclerView
+    lateinit var rvOffers: RecyclerView
+    lateinit var button : Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -35,26 +31,23 @@ class Explore : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explore, container, false)
+        view = inflater.inflate(R.layout.fragment_explore, container, false)
+
+        rvDestinations = view.findViewById(R.id.explore_rv_destinations)
+        rvOffers = view.findViewById(R.id.explore_rv_offers)
+        button = view.findViewById(R.id.explore_button)
+
+        return view
+    }
+
+    override fun onStart(){
+        super.onStart()
+        rvDestinations.setHasFixedSize(true)
+        val linearLayoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Explore.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Explore().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
