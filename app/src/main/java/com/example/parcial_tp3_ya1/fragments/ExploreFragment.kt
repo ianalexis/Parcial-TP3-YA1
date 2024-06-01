ca43f599d46c8db995275ccca5dde249f69a786c
morocontreras <contrerasaldana@hotmail.com>
2024-06-01 02:42:28 -0300

    Se agregan puntitos

--- app/src/main/java/com/example/parcial_tp3_ya1/fragments/ExploreFragment.kt
+++ app/src/main/java/com/example/parcial_tp3_ya1/fragments/ExploreFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
@@ -30,6 +31,7 @@ class ExploreFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
         arguments?.let {
 
         }
@@ -79,8 +81,18 @@ class ExploreFragment : Fragment() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.toolbar_explore, menu)
+        inflater.inflate(R.menu.drawer_menu, menu)
         super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                // Handle home button (hamburger menu)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     companion object {
