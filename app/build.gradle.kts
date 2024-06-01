827d8b81d3c8acb630c8ef36321185d7b8e28f2d
Ian Bassi <ian.bassi@outlook.com>
2024-06-01 03:01:18 -0300

    DarkMode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.glide)
+    implementation(libs.androidx.preference.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/parcial_tp3_ya1/fragments/PreferenceFragment.kt
@@ -0,0 +1,31 @@
+package com.example.parcial_tp3_ya1.fragments
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
+import com.example.parcial_tp3_ya1.R
+
+class PreferenceFragment : PreferenceFragmentCompat() {
+
+    companion object {
+        fun newInstance() = PreferenceFragment()
+    }
+
+    //private val viewModel: PreferenceViewModel by viewModels()
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.default_preference_screen, rootKey)
+
+        val nightMode = findPreference<SwitchPreference>(getString(R.string.preference_night_mode_key));
+        nightMode?.setDefaultValue(false)
+        nightMode?.setOnPreferenceChangeListener { _, newValue ->
+            if (newValue == true) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }
+            true
+        }
+    }
+}
