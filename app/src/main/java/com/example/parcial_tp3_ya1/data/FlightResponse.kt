82614bf5ae0ea348812a3fd0077ffd03cda6b8c2
Franco Leonel Cristillo <franco.frlcr@gmail.com>
2024-06-01 14:33:35 -0300

    Nav Update. Clean Data extra

--- app/src/main/java/com/example/parcial_tp3_ya1/data/FlightResponse.kt
+++ app/src/main/java/com/example/parcial_tp3_ya1/data/FlightResponse.kt
@@ -3,7 +3,7 @@ package com.example.parcial_tp3_ya1.data
 data class FlightResponse(
     val best_flights: List<BestFlight>?,
     val other_flights: List<OtherFlight>?,
-    val price_insights: PriceInsights?,
+ //   val price_insights: PriceInsights?,
   //  val search_metadata: SearchMetadata?,
     val search_parameters: SearchParameters?
 )
