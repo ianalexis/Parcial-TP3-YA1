86028f334501f3c2124382cd9ccbffb42a7f6055
Ian Bassi <ian.bassi@outlook.com>
2024-06-02 22:41:39 -0300

    CorazonClickeable y colores en etiquetas de codigos

--- app/src/main/java/com/example/parcial_tp3_ya1/holders/OffersDetailHolder.kt
+++ app/src/main/java/com/example/parcial_tp3_ya1/holders/OffersDetailHolder.kt
@@ -14,13 +14,30 @@ class OffersDetailHolder (view: View): RecyclerView.ViewHolder(view){
     val image = view.findViewById<ImageView>(R.id.card_detail_offers_image)
     val title = view.findViewById<TextView>(R.id.card_detail_offers_title)
     val desc = view.findViewById<TextView>(R.id.card_detail_offers_desc)
+    private val heart: ImageView = view.findViewById(R.id.card_offers_heart)
 
     init {
         this.view = view
+        heart.setOnClickListener {
+            toggleHeart(it)
+        }
     }
+
     fun render(model : OffersEntitie){
         Glide.with(view).load(model.image).into(image)
         title.text = model.longtitle
         desc.text = model.logndesc
+    }
+
+    fun toggleHeart(view: View) {
+        val heart = view as ImageView
+        val tag = heart.tag
+        if (tag == null || tag == R.drawable.card_detail_heart_empty) {
+            heart.setImageResource(R.drawable.card_detail_heart)
+            heart.tag = R.drawable.card_detail_heart
+        } else {
+            heart.setImageResource(R.drawable.card_detail_heart_empty)
+            heart.tag = R.drawable.card_detail_heart_empty
+        }
     }
 }
